7e018fbb58c9bf1e213921b88076bcc002922059
Muhammad Talha <talha424588@gmail.com>
2024-06-14 17:53:55 +0500

    use header page instead of navbar page in aap.js

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import Navbar from './components/navbar/Navbar';
 function App() {
   return (
     <div className="App">
-      {/* <Header/> */}
-      <Navbar/>
+      <Header/>
+      {/* <Navbar/> */}
     </div>
   );
 }
