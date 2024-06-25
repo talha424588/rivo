11f7ed220045b1feb7f06c89983d07d59c3f3ece
Muhammad Talha <talha424588@gmail.com>
2024-06-25 08:26:11 +0500

    header left part completed

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,23 +1,25 @@
 import Navbar from "../navbar/Navbar";
-import './Header.css';
-function Header()
-{
-    return(
-       <>
-        <Navbar/>
-        <div className="header">
-            <div className="container">
-                    <div className="content-section">
-                        <h1>
-                        Discover and Find Your Own Fashion!
-                        </h1>
-                        <p>Explore our curated collection of stylish clothing and accessories tailored to your unique taste.</p>
-                        <button>Explore Now</button>
-                    </div>
-                    <div className="image-section"></div>
-            </div>
+import "./Header.css";
+import header_image from "../../assets/header-image.png";
+function Header() {
+  return (
+    <div className="parent-container">
+      <Navbar />
+      <div className="header">
+        <div className="container">
+          <div className="content-section">
+            <h1>Discover and Find Your Own Fashion!</h1>
+            <p>
+              Explore our curated collection of stylish clothing and accessories
+              tailored to your unique taste.
+            </p>
+            <button>Explore Now</button>
+          </div>
+          <div className="image-section">
+          </div>
         </div>
-       </>
-    )
+      </div>
+    </div>
+  );
 }
 export default Header;
